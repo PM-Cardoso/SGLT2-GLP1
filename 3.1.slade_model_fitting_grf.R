5e919dc4729fdcf38042d8146ec24eb3f5d3015e
pml204 <pml204@exeter.ac.uk>
2022-08-23 16:18:28 +0100

    Adjust code to cope with new function for the validation of treatment effects.

--- 3.1.slade_model_fitting_grf.R
+++ 3.1.slade_model_fitting_grf.R
@@ -172,86 +172,44 @@ rate.val <- toc_function(dataset_model.matrix[-c(1:nrow(data_complete_routine_de
 #   ggtitle(paste0("Val GRF: TOC - ",signif(rate.val$estimate, 3)," [sd:", signif(rate.val$std.err, 3),"]"))
 
 
-############
-
-
-# predicted_observed_complete_routine_dev <- data_complete_routine_dev %>%
-#   select(posthba1c_final, prehba1cmmol, drugclass, drugline, ncurrtx, hba1cmonth, egfr_ckdepi, prealt, agetx, prebmi) %>%
-#   mutate(prealtlog = log(prealt)) %>%
-#   select(-prealt) %>%
-#   cbind(hba1c_diff = effects.dev$mean) %>%
-#   mutate(bestdrug = ifelse(hba1c_diff < 0, "SGLT2", "GLP1"),
-#          hba1c_diff.q = ntile(hba1c_diff, 10)) 
-# 
-# predicted_observed_complete_routine_dev_t1 <- predicted_observed_complete_routine_dev %>%
-#   ddply("hba1c_diff.q", dplyr::summarise,
-#         N = length(hba1c_diff),
-#         hba1c_diff.pred = mean(hba1c_diff))
-# 
-# 
-# 
-# mnumber = c(1:10)
-# models  <- as.list(1:10)
-# 
-# hba1c_diff.obs.unadj <- vector();lower.unadj <- vector();upper.unadj <- vector();hba1c_diff.obs.sim <- vector()
-# lower.sim <- vector();upper.sim <- vector();hba1c_diff.obs.adj <- vector();lower.adj <- vector();upper.adj <- vector() 
-# 
-# formula1 <- "posthba1c_final~factor(drugclass)"
-# formula2 <- "posthba1c_final~factor(drugclass)+prehba1cmmol+ncurrtx+drugline+rcs(hba1cmonth,3)+egfr_ckdepi+prealtlog"
-# formula3 <- "posthba1c_final~factor(drugclass)+rcs(prehba1cmmol,3)+ncurrtx+drugline+rcs(hba1cmonth,3)+rcs(egfr_ckdepi,3)+rcs(prealtlog,3)+rcs(agetx,3)+rcs(prebmi,3)"
-# f <- as.list(c(formula1,formula2,formula3))
-# 
-# # #Unadj
-# for(i in mnumber) {
-#   models[[i]] <- lm(as.formula(formula1),data=predicted_observed_complete_routine_dev,subset=hba1c_diff.q==i)
-#   hba1c_diff.obs.unadj <- append(hba1c_diff.obs.unadj,models[[i]]$coefficients[2])
-#   confint_all <- confint(models[[i]], levels=0.95)
-#   lower.unadj <- append(lower.unadj,confint_all[2,1])
-#   upper.unadj <- append(upper.unadj,confint_all[2,2])
-# }
-# # #Simple 
-# for(i in mnumber) {
-#   models[[i]] <- lm(as.formula(formula2),data=predicted_observed_complete_routine_dev,subset=hba1c_diff.q==i)
-#   hba1c_diff.obs.sim <- append(hba1c_diff.obs.sim,models[[i]]$coefficients[2])
-#   confint_all <- confint(models[[i]], levels=0.95)
-#   lower.sim <- append(lower.sim,confint_all[2,1])
-#   upper.sim <- append(upper.sim,confint_all[2,2])
-# }
-# #Full
-# for(i in mnumber) {
-#   models[[i]] <- lm(as.formula(formula3),data=predicted_observed_complete_routine_dev,subset=hba1c_diff.q==i)
-#   hba1c_diff.obs.adj <- append(hba1c_diff.obs.adj,models[[i]]$coefficients[2])
-#   confint_all <- confint(models[[i]], levels=0.95)
-#   lower.adj <- append(lower.adj,confint_all[2,1])
-#   upper.adj <- append(upper.adj,confint_all[2,2])
-# }
-# 
-# #Final data.frame  
-# t1 <- data.frame(predicted_observed_complete_routine_dev_t1,cbind(hba1c_diff.obs.unadj,lower.unadj,upper.unadj,
-#                                                                   hba1c_diff.obs.sim,lower.sim,upper.sim,
-#                                                                   hba1c_diff.obs.adj,lower.adj,upper.adj))
+#######
 
 
 predicted_observed_complete_routine_dev <- data_complete_routine_dev %>%
-  # select(posthba1c_final, prehba1cmmol, drugclass, drugline, ncurrtx, hba1cmonth, egfr_ckdepi, prealt, agetx, prebmi) %>%
-  # mutate(prealtlog = log(prealt)) %>%
-  # select(-prealt) %>%
   cbind(hba1c_diff = effects.dev$mean) %>%
   mutate(bestdrug = ifelse(hba1c_diff < 0, "SGLT2", "GLP1"),
          hba1c_diff.q = ntile(hba1c_diff, 10)) 
 
 
-t1 <- effects_calibration(predicted_observed_complete_routine_dev)
+t1 <- effects_calibration(predicted_observed_complete_routine_dev, 
+                          dataset = "Dev", 
+                          model = "Linear", 
+                          formula = "formula1")
+
+
+t2 <- effects_calibration(predicted_observed_complete_routine_dev, 
+                          dataset = "Dev", 
+                          model = "Linear", 
+                          formula = "formula2")
+
+
+t3 <- effects_calibration(predicted_observed_complete_routine_dev, 
+                          dataset = "Dev", 
+                          model = "Linear", 
+                          formula = "formula3")
 
 
 
 #simple adj
 plotdata_1 <- t1 %>% dplyr::mutate(obs=hba1c_diff.obs.unadj,lci=lower.unadj,uci=upper.unadj)
-# plotdata_2 <- t1 %>% dplyr::mutate(obs=hba1c_diff.obs.sim,lci=lower.sim,uci=upper.sim)
-# plotdata_3 <- t1 %>% dplyr::mutate(obs=hba1c_diff.obs.adj,lci=lower.adj,uci=upper.adj)
+plotdata_2 <- t2 %>% dplyr::mutate(obs=hba1c_diff.obs.unadj,lci=lower.unadj,uci=upper.unadj)
+plotdata_3 <- t3 %>% dplyr::mutate(obs=hba1c_diff.obs.unadj,lci=lower.unadj,uci=upper.unadj)
 
 
-plot_predicted_observed <- hte_plot(plotdata_1,"hba1c_diff.pred","obs","lci","uci") 
+
+plot_predicted_observed_1 <- hte_plot(plotdata_1,"hba1c_diff.pred","obs","lci","uci") 
+plot_predicted_observed_2 <- hte_plot(plotdata_2,"hba1c_diff.pred","obs","lci","uci") 
+plot_predicted_observed_3 <- hte_plot(plotdata_3,"hba1c_diff.pred","obs","lci","uci") 
 
 
 
@@ -280,7 +238,8 @@ rate.dev$TOC %>%
   ggtitle(paste0("Dev GRF: TOC - ",signif(rate.dev$estimate, 3)," [sd:", signif(rate.dev$std.err, 3),"]"))
 
 
-plot_predicted_observed
+cowplot::plot_grid(cowplot::plot_grid(plot_predicted_observed_1, plot_predicted_observed_2, plot_predicted_observed_3, ncol = 3),
+                   ncol = 1, nrow = 2)
 
 
 dev.off()
