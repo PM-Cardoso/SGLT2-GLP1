ca07ce1f0b3df65b07dd7b2e647f35b4847c111e
pml204 <pml204@exeter.ac.uk>
2022-08-23 16:18:45 +0100

    Removal of functions from code. Adjust code to cope with new function for the validation of treatment effects.

--- 3.2.slade_model_fitting_bcf.R
+++ 3.2.slade_model_fitting_bcf.R
@@ -49,26 +49,7 @@ load(paste0(output_path, "/datasets/cprd_19_sglt2glp1_valcohort.Rda"))
 ###############################################################################
 ###############################################################################
 
-#Function to output HTE by subgroup
-hte_plot <- function(data,pred,obs,obslowerci,obsupperci) {
-  
-  #ymin <- min(data$lci); ymax <- max(data$uci);yminr  <- 2*round(ymin/2);  ymaxr <- 2*round(ymax/2)
-  ymin  <- -15;  ymax <- 15
-  
-  ggplot(data=data,aes_string(x=pred,y=obs)) +
-    geom_point(alpha=1) + theme_bw() +
-    geom_errorbar(aes_string(ymin=obslowerci, ymax=obsupperci), colour="black", width=.1) +
-    ylab("Observed HbA1c difference (mmol/mol)") + xlab("Predicted HbA1c difference (mmol/mol)") +
-    scale_x_continuous(limits=c(ymin,ymax),breaks=c(seq(ymin,ymax,by=2))) +
-    scale_y_continuous(limits=c(ymin,ymax),breaks=c(seq(ymin,ymax,by=2))) +
-    # scale_x_continuous(limits=c(ymin,ymax),breaks=c(seq(yminr,ymaxr,by=2))) +
-    # scale_y_continuous(limits=c(ymin,ymax),breaks=c(seq(yminr,ymaxr,by=2))) +
-    geom_abline(intercept=0,slope=1, color="red", lwd=0.75) + ggtitle("") +
-    geom_vline(xintercept=0, linetype="dashed", color = "grey60") + geom_hline(yintercept=0, linetype="dashed", color = "grey60") 
-}
-
-
-
+source("0.0.slade_functions.R")
 
 ############################# BCF
 ### Complete model of only routine data, no propensity score (n: 9866))
@@ -153,71 +134,41 @@ effects.dev <- cbind(mean = post$tau %>% colMeans()) %>%
 #########
 
 
-
-
 predicted_observed_complete_routine_dev <- data_complete_routine_dev %>%
-  select(posthba1c_final, prehba1cmmol, drugclass, drugline, ncurrtx, hba1cmonth, egfr_ckdepi, prealt, agetx, prebmi) %>%
-  mutate(prealtlog = log(prealt)) %>%
-  select(-prealt) %>%
   cbind(hba1c_diff = effects.dev$mean) %>%
   mutate(bestdrug = ifelse(hba1c_diff < 0, "SGLT2", "GLP1"),
          hba1c_diff.q = ntile(hba1c_diff, 10)) 
 
-predicted_observed_complete_routine_dev_t1 <- predicted_observed_complete_routine_dev %>%
-  ddply("hba1c_diff.q", dplyr::summarise,
-        N = length(hba1c_diff),
-        hba1c_diff.pred = mean(hba1c_diff))
+
+t1 <- effects_calibration(predicted_observed_complete_routine_dev, 
+                          dataset = "Dev", 
+                          model = "Linear", 
+                          formula = "formula1")
 
 
+t2 <- effects_calibration(predicted_observed_complete_routine_dev, 
+                          dataset = "Dev", 
+                          model = "Linear", 
+                          formula = "formula2")
 
-mnumber = c(1:10)
-models  <- as.list(1:10)
 
-hba1c_diff.obs.unadj <- vector();lower.unadj <- vector();upper.unadj <- vector();hba1c_diff.obs.sim <- vector()
-lower.sim <- vector();upper.sim <- vector();hba1c_diff.obs.adj <- vector();lower.adj <- vector();upper.adj <- vector() 
+t3 <- effects_calibration(predicted_observed_complete_routine_dev, 
+                          dataset = "Dev", 
+                          model = "Linear", 
+                          formula = "formula3")
 
-formula1 <- "posthba1c_final~factor(drugclass)"
-formula2 <- "posthba1c_final~factor(drugclass)+prehba1cmmol+ncurrtx+drugline+rcs(hba1cmonth,3)+egfr_ckdepi+prealtlog"
-formula3 <- "posthba1c_final~factor(drugclass)+rcs(prehba1cmmol,3)+ncurrtx+drugline+rcs(hba1cmonth,3)+rcs(egfr_ckdepi,3)+rcs(prealtlog,3)+rcs(agetx,3)+rcs(prebmi,3)"
-f <- as.list(c(formula1,formula2,formula3))
 
-# #Unadj
-for(i in mnumber) {
-  models[[i]] <- lm(as.formula(formula1),data=predicted_observed_complete_routine_dev,subset=hba1c_diff.q==i)
-  hba1c_diff.obs.unadj <- append(hba1c_diff.obs.unadj,models[[i]]$coefficients[2])
-  confint_all <- confint(models[[i]], levels=0.95)
-  lower.unadj <- append(lower.unadj,confint_all[2,1])
-  upper.unadj <- append(upper.unadj,confint_all[2,2])
-}
-# #Simple 
-for(i in mnumber) {
-  models[[i]] <- lm(as.formula(formula2),data=predicted_observed_complete_routine_dev,subset=hba1c_diff.q==i)
-  hba1c_diff.obs.sim <- append(hba1c_diff.obs.sim,models[[i]]$coefficients[2])
-  confint_all <- confint(models[[i]], levels=0.95)
-  lower.sim <- append(lower.sim,confint_all[2,1])
-  upper.sim <- append(upper.sim,confint_all[2,2])
-}
-#Full
-for(i in mnumber) {
-  models[[i]] <- lm(as.formula(formula3),data=predicted_observed_complete_routine_dev,subset=hba1c_diff.q==i)
-  hba1c_diff.obs.adj <- append(hba1c_diff.obs.adj,models[[i]]$coefficients[2])
-  confint_all <- confint(models[[i]], levels=0.95)
-  lower.adj <- append(lower.adj,confint_all[2,1])
-  upper.adj <- append(upper.adj,confint_all[2,2])
-}
-
-#Final data.frame  
-t1 <- data.frame(predicted_observed_complete_routine_dev_t1,cbind(hba1c_diff.obs.unadj,lower.unadj,upper.unadj,
-                                                                  hba1c_diff.obs.sim,lower.sim,upper.sim,
-                                                                  hba1c_diff.obs.adj,lower.adj,upper.adj))
 
 #simple adj
 plotdata_1 <- t1 %>% dplyr::mutate(obs=hba1c_diff.obs.unadj,lci=lower.unadj,uci=upper.unadj)
-plotdata_2 <- t1 %>% dplyr::mutate(obs=hba1c_diff.obs.sim,lci=lower.sim,uci=upper.sim)
-plotdata_3 <- t1 %>% dplyr::mutate(obs=hba1c_diff.obs.adj,lci=lower.adj,uci=upper.adj)
+plotdata_2 <- t2 %>% dplyr::mutate(obs=hba1c_diff.obs.unadj,lci=lower.unadj,uci=upper.unadj)
+plotdata_3 <- t3 %>% dplyr::mutate(obs=hba1c_diff.obs.unadj,lci=lower.unadj,uci=upper.unadj)
 
 
-plot_predicted_observed <- hte_plot(plotdata_3,"hba1c_diff.pred","obs","lci","uci") 
+
+plot_predicted_observed_1 <- hte_plot(plotdata_1,"hba1c_diff.pred","obs","lci","uci") 
+plot_predicted_observed_2 <- hte_plot(plotdata_2,"hba1c_diff.pred","obs","lci","uci") 
+plot_predicted_observed_3 <- hte_plot(plotdata_3,"hba1c_diff.pred","obs","lci","uci") 
 
 
 
@@ -241,7 +192,8 @@ prop.score$fitted.values %>%
 
 hist_plot(effects.dev,-2.5,2.3,1100, "Dev BCF: treatment effect", -15, 20)
 
-plot_predicted_observed
+cowplot::plot_grid(cowplot::plot_grid(plot_predicted_observed_1, plot_predicted_observed_2, plot_predicted_observed_3, ncol = 3),
+                   ncol = 1, nrow = 2)
 
 dev.off()
 
