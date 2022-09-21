c8152af678594ba6e2f7f9148e3d4f7397101419
pml204 <pml204@exeter.ac.uk>
2022-09-21 12:22:03 +0100

    Add extra options for visualisation of results: target probability, outcome interval, probability density. Add "Missing" options to sex and smoking. Adjust code to predict with missing data. Add units to most variable titles. Add panels for visualisation of results. Adjust the way predictions are calculated. Add code for when probability is 100%, change to >99%. When probability is 0%, change to <0.1% Add code for calculating/plotting predicted weight change.

--- 09.01.shiny_app_prob_target.R
+++ 09.01.shiny_app_prob_target.R
@@ -4,6 +4,10 @@
 ##      achieving a target HbA1c.
 ####################
 
+# if probability says 100%, change to >99%
+# find patients with several outcomes for examples
+
+
 
 ## increase memory usage to 3gb of RAM
 options(java.parameters = "-Xmx3000m")
@@ -34,7 +38,8 @@ ui <- fluidPage(
            radioButtons("output_type",
                         label = h3("Output type:"),
                         choices = list("Absolute probability" = 1,
-                                       "Probability interval" = 2),
+                                       "Outcome interval" = 2,
+                                       "Probability density" = 3),
                         selected = 1)
            ),
     
@@ -43,10 +48,11 @@ ui <- fluidPage(
                        label = h6("Sex"),
                        choices = list("",
                                       "Male",
-                                      "Female"),
+                                      "Female",
+                                      "Missing"),
                        selected = NULL),
            numericInput("egfr_num",
-                        label = h6("eGFR"),
+                        label = h6("eGFR (ml/min/1.73 m2)"),
                         value = 96,
                         min = 40,
                         max = 170),
@@ -67,10 +73,11 @@ ui <- fluidPage(
                        choices = list("",
                                       "Non-smoker",
                                       "Ex-smoker",
-                                      "Active smoker"),
+                                      "Active smoker",
+                                      "Missing"),
                        selected = NULL),
            numericInput("age_num",
-                        label = h6("Age"),
+                        label = h6("Age (years)"),
                         value = 65,
                         min = 18,
                         max = 120),
@@ -92,24 +99,24 @@ ui <- fluidPage(
                         min = 0,
                         max = 1500),
            numericInput("t2dmduration_num",
-                        label = h6("Time to prescription"),
+                        label = h6("Time to prescription (years)"),
                         value = 8.37,
                         min = 0,
                         max = 50),
            numericInput("alb_num",
-                        label = h6("ALB"),
+                        label = h6("Albumin (mg/g)"),
                         value = 40,
                         min = 0,
                         max = 70),
            numericInput("sys_num",
-                        label = h6("Systolic Blood Pressure"),
+                        label = h6("Systolic Blood Pressure (mmHg)"),
                         value = 120,
                         min = 50,
                         max = 250)
            ),
     column(2,
            numericInput("ast_num",
-                        label = h6("Aspartate aminotransferase"),
+                        label = h6("Aspartate aminotransferase (U/L)"),
                         value = 28.9,
                         min = 0,
                         max = 400),
@@ -122,30 +129,33 @@ ui <- fluidPage(
                         label = h6("CVD score"),
                         value = 0.01,
                         min = 0,
-                        max = 1)
+                        max = 25)
            )
   ),
   br(), 
   
-  
   # Conditional panel in case output_type = 1 (absolute probability)
   conditionalPanel(condition = "input.output_type == 1",
                    
-    # title of panel
-    h4("12-month probability of achieving target HbA1c"),
-    plotOutput("probability_plot", height = "160px", width = "400px")
+                   # title of panel
+                   plotOutput("absolute_plot", height = "160px", width = "800px")
   ),
   
-  # Conditional panel in case output_type = 2 (probability interval)
+  # Conditional panel in case output_type = 2 (outcome interval)
   conditionalPanel(condition = "input.output_type == 2",
-    
-    # title of panel
-    h4("Estimated 12-month average HbA1c"),
-    plotOutput("histogram_plot", height = "250px", width = "400px")
-    
                    
+                   # title of panel
+                   plotOutput("outcome_interval", height = "160px", width = "800px")
+  ),
+  
+  
+  # Conditional panel in case output_type = 3 (probability density)
+  conditionalPanel(condition = "input.output_type == 3",
                    
-  )
+                   # title of panel
+                   plotOutput("probability_plot", height = "250px", width = "800px")
+  )   
+            
   
 )
 
@@ -172,10 +182,14 @@ server <- function(input, output, session) {
     patient$prealt <- as.numeric(input$alt_num)
     # prehba1cmmol
     patient$prehba1cmmol <- as.numeric(input$hba1c_num)
-    # score
-    patient$score <- as.numeric(input$score_num)
+    # score.excl.mi
+    patient$score.excl.mi <- as.numeric(input$score_num)
     # Category (smoker)
-    patient$Category <- factor(input$smoker_select, levels = c("Active smoker", "Ex-smoker", "Non-smoker"))
+    if (input$smoker_select == "Missing") {
+      patient$Category <- factor(NA, levels = c("Active smoker", "Ex-smoker", "Non-smoker"))
+    } else {
+      patient$Category <- factor(input$smoker_select, levels = c("Active smoker", "Ex-smoker", "Non-smoker"))
+    }
     # drugline
     patient$drugline <- factor("2", levels = c("2", "3", "4", "5"))
     # ncurrtx
@@ -187,8 +201,10 @@ server <- function(input, output, session) {
     # malesex
     if (input$sex_select == "Male") {
       patient$malesex <- factor("1", levels = c("0", "1"))
-    } else {
+    } else if (input$sex_select == "Female") {
       patient$malesex <- factor("0", levels = c("0", "1"))
+    } else {
+      patient$malesex <- factor(NA, levels = c("0", "1"))
     }
     # prehdl
     patient$prehdl <- as.numeric(input$hdl_num)
@@ -215,7 +231,7 @@ server <- function(input, output, session) {
     session$reload()
   })
   
-  posteriors <- eventReactive(input$calculate, {
+  posteriors_hba1c <- eventReactive(input$calculate, {
     
     patient <- patient()
     
@@ -226,25 +242,42 @@ server <- function(input, output, session) {
                      patient %>%
                        mutate(drugclass = factor("GLP1", levels = c("GLP1", "SGLT2"))))
     
-    posteriors <-  bartMachine::bart_machine_get_posterior(bart_model, patient)
+    posteriors_hba1c <-  bartMachine::bart_machine_get_posterior(bart_model, patient)
     
+    posteriors_hba1c
   })
+  
+  posteriors_weight <- eventReactive(input$calculate, {
+    
+    patient <- patient()
+    
+    bart_model_weights <- readRDS("Shiny App/bart_model_weights.rds")
+    
+    patient <- patient %>%
+      mutate(drugclass = factor("SGLT2", levels = c("GLP1", "SGLT2")))
+    patient <- rbind(patient,
+                     patient %>%
+                       mutate(drugclass = factor("GLP1", levels = c("GLP1", "SGLT2"))))
+    
+    posteriors_weight <-  bartMachine::bart_machine_get_posterior(bart_model_weights, patient)
+    
+    posteriors_weight
+  })
+  
   # make output plot of probability SGLT2
-  output$probability_plot <- renderPlot({
+  output$absolute_plot <- renderPlot({
     
     # load target HbA1c 
     target <- target()
     
     # load predictions
-    posteriors <- posteriors()
+    posteriors_hba1c <- posteriors_hba1c()
     
     # SGLT2 probability of achieving target and surpassing it
-    probability_SGLT2 <- length(which(posteriors$y_hat_posterior_samples[1,] < target))/length(posteriors$y_hat_posterior_samples[1,])
+    probability_SGLT2 <- length(which(posteriors_hba1c$y_hat_posterior_samples[1,] < target))/length(posteriors_hba1c$y_hat_posterior_samples[1,])
     
     # GLP1 probability of achieving target and surpassing it
-    probability_GLP1 <- length(which(posteriors$y_hat_posterior_samples[2,] < target))/length(posteriors$y_hat_posterior_samples[2,])
-    
-    
+    probability_GLP1 <- length(which(posteriors_hba1c$y_hat_posterior_samples[2,] < target))/length(posteriors_hba1c$y_hat_posterior_samples[2,])
     
     # reshape data
     df <- data.frame(
@@ -254,41 +287,173 @@ server <- function(input, output, session) {
       gather(key, val) %>%
       mutate(
         key = factor(key, levels = c("GLP1", "SGLT2")),
-        Total = 100)
+        Total = 100) %>%
+      mutate(label = paste0(key, ": ", val,"%"))
+    
+    if (probability_SGLT2 > 0.999) {
+      df$label[1] <- "SGLT2: > 99.9%"
+    } else if (probability_SGLT2 < 0.001) {
+      df$label[1] <- "SGLT2: <0.1%"
+    }
+    if (probability_GLP1 > 0.999) {
+      df$label[2] <- "GLP1: > 99.9%"
+    } else if (probability_GLP1 < 0.001) {
+      df$label[2] <- "GLP1: <0.1%"
+    }
 
-    plot <- ggplot(df, aes(key, val)) +
-      geom_col(fill = c("#f1a340","#998ec3")) +
-      geom_col(aes(y = Total), alpha = 0.5) +
+    plot_target <- ggplot(df, aes(key, val)) +
+      geom_col(fill = c("#f1a340","red")) +
+      geom_col(aes(y = Total), colour = "white", alpha = 0.1) +
       geom_text(
-        aes(y = 5, label = paste0(key, ": ", val,"%")),
+        aes(y = 5, label = label),
         hjust = 0,
-        fontface = "bold",
-        colour = "white",
+        # fontface = "bold",
+        colour = "black",
         size = 10) +
+      ggtitle("12-month probability of achieving target HbA1c") +
       coord_flip() +
-      scale_colour_manual(values=c("#998ec3","#f1a340")) +
+      scale_colour_manual(values=c("red","#f1a340")) +
       theme_minimal() +
       theme(
-        axis.title = element_blank(),
-        axis.text = element_blank(),
-        axis.ticks = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(colour = "white"),
+        axis.ticks.x = element_line(colour = "white"),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        panel.background = element_blank())
+        panel.background = element_blank(),
+        title = element_text(size = 13))
 
-    plot
+    posteriors_weight <- posteriors_weight()
+
+    plot_weight <- cbind(val = posteriors_weight$y_hat, key = c("SGLT2", "GLP1")) %>%
+      as.data.frame() %>%
+      mutate(val = as.numeric(val)) %>%
+      ggplot(aes(x = key, y = val, fill = key)) +
+      geom_col() +
+      geom_hline(aes(yintercept = 0), linetype = "dashed", alpha = 0.5) +
+      geom_text(
+        aes(y = val + 0.1, label = paste0(key, ": ", signif(val, digits = 2), " kg")),
+        hjust = 0,
+        # fontface = "bold",
+        colour = "black",
+        size = 10) +
+      scale_fill_manual(values=c("red","#f1a340")) +
+      coord_flip() +
+      theme_classic() +
+      ggtitle("Estimated 6-month weight change") +
+      theme(axis.title.y = element_blank(),
+            axis.line.y = element_blank(),
+            axis.ticks.y = element_blank(),
+            axis.text.y = element_blank(),
+            axis.title.x = element_blank(),
+            legend.position = "none",
+            title = element_text(size = 13))
+    
+    cowplot::plot_grid(
+      
+      plot_target,
+      
+      plot_weight,
+      
+      ncol = 2, nrow = 1
+    )
     
   })
-
-  output$histogram_plot <- renderPlot({
+  
+  output$outcome_interval <- renderPlot({
+    
+    # load predictions
+    posteriors_hba1c <- posteriors_hba1c()
+    
+    plot_outcome_interval <- cbind(
+      `5%` = apply(posteriors_hba1c$y_hat_posterior_samples, MARGIN = 1, function(x) quantile(c(x), probs = c(0.05))),
+      `50%` = apply(posteriors_hba1c$y_hat_posterior_samples, MARGIN = 1, function(x) quantile(c(x), probs = c(0.50))),
+      `95%` = apply(posteriors_hba1c$y_hat_posterior_samples, MARGIN = 1, function(x) quantile(c(x), probs = c(0.95))),
+      key = c("SGLT2", "GLP1")
+    ) %>%
+      as.data.frame() %>%
+      mutate(`5%` = as.numeric(`5%`),
+             `50%` = as.numeric(`50%`),
+             `95%` = as.numeric(`95%`),
+             key = as.factor(key)) %>%
+      ggplot(aes(x = key, y = `50%`)) +
+      geom_crossbar(aes(ymin = `5%`, ymax = `95%`, fill = key, colour = key)) +
+      geom_text(
+        aes(y = `50%`-1.3, label = key),
+        hjust = 0,
+        # fontface = "bold",
+        colour = "black",
+        size = 10) +
+      scale_fill_manual(values=c("red","#f1a340")) +
+      scale_colour_manual(values=c("red","#f1a340")) +
+      coord_flip() +
+      theme_classic() +
+      ggtitle("Estimated 12-month average HbA1c") +
+      theme(axis.title.y = element_blank(),
+            axis.line.y = element_blank(),
+            axis.ticks.y = element_blank(),
+            axis.text.y = element_blank(),
+            axis.title.x = element_blank(),
+            legend.position = "none",
+            title = element_text(size = 13))
+    
+    posteriors_weight <- posteriors_weight()
+    
+    plot_weight_interval <- cbind(
+      `5%` = apply(posteriors_weight$y_hat_posterior_samples, MARGIN = 1, function(x) quantile(c(x), probs = c(0.05))),
+      `50%` = apply(posteriors_weight$y_hat_posterior_samples, MARGIN = 1, function(x) quantile(c(x), probs = c(0.50))),
+      `95%` = apply(posteriors_weight$y_hat_posterior_samples, MARGIN = 1, function(x) quantile(c(x), probs = c(0.95))),
+      key = c("SGLT2", "GLP1")
+    ) %>%
+      as.data.frame() %>%
+      mutate(`5%` = as.numeric(`5%`),
+             `50%` = as.numeric(`50%`),
+             `95%` = as.numeric(`95%`),
+             key = as.factor(key)) %>%
+      ggplot(aes(x = key, y = `50%`)) +
+      geom_crossbar(aes(ymin = `5%`, ymax = `95%`, fill = key, colour = key)) +
+      geom_text(
+        aes(y = `50%`-1, label = key),
+        hjust = 0,
+        # fontface = "bold",
+        colour = "black",
+        size = 10) +
+      scale_fill_manual(values=c("red","#f1a340")) +
+      scale_colour_manual(values=c("red","#f1a340")) +
+      coord_flip() +
+      theme_classic() +
+      ggtitle("Estimated 6-month weight change") +
+      theme(axis.title.y = element_blank(),
+            axis.line.y = element_blank(),
+            axis.ticks.y = element_blank(),
+            axis.text.y = element_blank(),
+            axis.title.x = element_blank(),
+            legend.position = "none",
+            title = element_text(size = 13))
+    
+    cowplot::plot_grid(
+      
+      plot_outcome_interval,
+      
+      plot_weight_interval,
+      
+      ncol = 2, nrow = 1
+    )
+    
+  })
+  
+  output$probability_plot <- renderPlot({
     
     # load target HbA1c 
     target <- target()
     
     # load predictions
-    posteriors <- posteriors()
+    posteriors_hba1c <- posteriors_hba1c()
     
-    plot <- posteriors$y_hat_posterior_samples %>%
+    plot_outcome <- posteriors_hba1c$y_hat_posterior_samples %>%
       t() %>%
       as.data.frame() %>%
       set_names("SGLT2", "GLP1") %>%
@@ -296,14 +461,45 @@ server <- function(input, output, session) {
       ggplot() +
       geom_density(aes(x = value, fill = key), alpha = 0.5, colour = "black") +
       labs(x = "Average HbA1c (mmol/mol)", y = "Density") +
-      scale_fill_manual(values=c("#998ec3","#f1a340"))+
+      scale_fill_manual(values=c("red","#f1a340")) +
+      ggtitle("Estimated 12-month average HbA1c") +
       theme_classic() +
-      theme(legend.position = c(0.80, 0.87)) +
-      theme(legend.title = element_blank(),
+      theme(legend.position = c(0.80, 0.87),
+            legend.title = element_blank(),
             axis.text.y = element_blank(),
-            axis.ticks.y = element_blank())
+            axis.ticks.y = element_blank(),
+            title = element_text(size = 13))
       
-    plot
+    posteriors_weight <- posteriors_weight()
+    
+    plot_weight_hist <- posteriors_weight$y_hat_posterior_samples %>%
+      t() %>%
+      as.data.frame() %>%
+      set_names("SGLT2", "GLP1") %>%
+      gather(key, value) %>%
+      ggplot() +
+      geom_density(aes(x = value, fill = key), alpha = 0.5, colour = "black") +
+      labs(x = "Average weight reduction (kg)", y = "Density") +
+      scale_fill_manual(values=c("red","#f1a340")) +
+      ggtitle("Estimated 6-month weight change") +
+      theme_classic() +
+      theme(legend.title = element_blank(),
+            axis.title.y = element_blank(),
+            axis.line.y = element_blank(),
+            axis.text.y = element_blank(),
+            axis.ticks.y = element_blank(),
+            title = element_text(size = 13),
+            legend.position = "none")
+    
+    cowplot::plot_grid(
+      
+      plot_outcome,
+      
+      plot_weight_hist,
+      
+      ncol = 2, nrow = 1
+    )
+    
     
   })
   
@@ -313,7 +509,7 @@ server <- function(input, output, session) {
 runGadget(ui, server, viewer = browserViewer(browser = getOption("browser")))
 
 
-# ## Code for testing purposes 
+# ## Code for testing purposes
 # 
 # # Used in slade to ensure the library being used is my personal library
 # .libPaths(.libPaths()[c(2,1,3)])
@@ -325,7 +521,7 @@ runGadget(ui, server, viewer = browserViewer(browser = getOption("browser")))
 # library(tidyverse)
 # library(bartMachine)
 # 
-# bart_model_final <- readRDS("Samples/SGLT2-GLP1/Final_model/With_grf_no_prop/bart_model_final.rds")
+# bart_model_final <- readRDS("Samples/SGLT2-GLP1/Final_model/cvd_new/bart_model_final.rds")
 # 
 # 
 # patient <- cbind(
@@ -334,7 +530,7 @@ runGadget(ui, server, viewer = browserViewer(browser = getOption("browser")))
 #   hba1cmonth = 12,
 #   prealt = 23,
 #   prehba1cmmol = 65,
-#   score = 0.01,
+#   score.excl.mi = 0.01,
 #   Category = "Non-smoker",
 #   drugline = "2",
 #   ncurrtx = "1",
@@ -356,7 +552,7 @@ runGadget(ui, server, viewer = browserViewer(browser = getOption("browser")))
 #          hba1cmonth = as.numeric(hba1cmonth),
 #          prealt = as.numeric(prealt),
 #          prehba1cmmol = as.numeric(prehba1cmmol),
-#          score = as.numeric(score),
+#          score.excl.mi = as.numeric(score.excl.mi),
 #          Category = factor(Category, levels = c("Active smoker", "Ex-smoker", "Non-smoker")),
 #          drugline = factor(drugline, levels = c("2", "3", "4", "5")),
 #          ncurrtx = factor(ncurrtx, levels = c("0", "1", "2", "3")),
@@ -386,3 +582,24 @@ runGadget(ui, server, viewer = browserViewer(browser = getOption("browser")))
 # summary(t(posteriors_SGLT2$y_hat_posterior_samples))
 # 
 # saveRDS(test, "bart_model.rds")
+# 
+# # model for weight reduction
+# 
+# bart_model_weights <- readRDS("Samples/SGLT2-GLP1/Weight_reduction/bart_model_weight.rds")
+# 
+# test <- bart_model_weights
+# 
+# library(synthpop)
+# 
+# syn.dataset <- syn(cbind(test$X, y = test$y))
+# test$X <- syn.dataset$syn[1:100,-21]
+# test$y <- syn.dataset$syn[1:100,"y"]
+# 
+# # posteriors
+# posteriors_weights <- bartMachine::bart_machine_get_posterior(test, patient)
+# summary(t(posteriors_weights$y_hat_posterior_samples))
+# 
+# saveRDS(test, "bart_model_weights.rds")
+
+
+
