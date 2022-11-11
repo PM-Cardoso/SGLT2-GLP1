c3a5628db69307d5a840abfb2bfacd2676e9d0be
pml204 <pml204@exeter.ac.uk>
2022-11-11 09:55:32 +0000

    Updating script to the current modelling procedure of ps model.

--- 11.04.slade_aurum_ps_model.R
+++ 11.04.slade_aurum_ps_model.R
@@ -6,12 +6,9 @@
 ##    - Refit BART PS model
 ####################
 
-# Used in slade to ensure the library being used is my personal library
-.libPaths(.libPaths()[c(2,1,3)])
-
 
 ## increase memery usage to 50gb of RAM
-options(java.parameters = "-Xmx50g")
+options(java.parameters = "-Xmx100g")
 
 library(tidyverse)
 library(bartMachine)
@@ -64,55 +61,30 @@ ps.model.train <- set_up_data_sglt2_glp1(dataset.type = "ps.model.train")
 ### Fit a propensity score model to all variables in dataset
 ########
 
-## Fit initial model using all the available variables to estimate HbA1c outcome
-if (class(try(
-
-  bart_ps_model <- readRDS(paste0(output_path, "/ps_model/bart_ps_model.rds"))
-
-  , silent = TRUE)) == "try-error") {
-
-  bart_ps_model <- bartMachine::bartMachine(X = ps.model.train %>%
-                                              select(-patid,
-                                                     -pated,
-                                                     -drugclass),
-                                            y = ps.model.train[,"drugclass"] %>%
-                                              unlist(),
-                                            use_missing_data = TRUE,
-                                            num_trees = 50,
-                                            num_burn_in = 15000,
-                                            num_iterations_after_burn_in = 3000,
-                                            serialize = TRUE)
-
-  saveRDS(bart_ps_model, paste0(output_path, "/ps_model/bart_ps_model.rds"))
-
-}
-
 
 # Check the optimal number of trees
 ## Does not work for classification
 
 
-# Cross validation of model
 if (class(try(
   
-  bart_ps_model_cv <- readRDS(paste0(output_path, "/ps_model/bart_ps_model_cv.rds"))
+  bart_ps_model <- readRDS(paste0(output_path, "/ps_model/bart_ps_model.rds"))
   
   , silent = TRUE)) == "try-error") {
   
-  bart_ps_model_cv <- bartMachine::bartMachineCV(X = ps.model.train %>%
+  bart_ps_model <- bartMachine::bartMachine(X = ps.model.train %>%
                                                    select(-patid,
                                                           -pated,
                                                           -drugclass),
                                                  y = ps.model.train[,"drugclass"] %>%
                                                    unlist(),
-                                                 num_tree_cvs = c(50, 100, 150, 200),
-                                                 k_cvs = c(2, 3),
+                                                 num_trees = 50,
                                                  use_missing_data = TRUE,
                                                  num_burn_in = 15000,
-                                                 num_iterations_after_burn_in = 3000,
+                                                 num_iterations_after_burn_in = 10000,
                                                  serialize = TRUE)
   
-  saveRDS(bart_ps_model_cv, paste0(output_path, "/ps_model/bart_ps_model_cv.rds"))
+  saveRDS(bart_ps_model, paste0(output_path, "/ps_model/bart_ps_model.rds"))
   
 }
 
@@ -159,19 +131,18 @@ if (class(try(
                                                       ),
                                                   y = ps.model.train[,"drugclass"] %>%
                                                     unlist(),
-                                                  num_tree_cvs = c(50, 100, 150, 200),
-                                                  k_cvs = c(2, 3),
+                                                  num_trees = 50,
                                                   use_missing_data = TRUE,
                                                   num_burn_in = 15000,
-                                                  num_iterations_after_burn_in = 3000,
-                                                  serialize = FALSE)
+                                                  num_iterations_after_burn_in = 10000,
+                                                  serialize = TRUE)
 
   saveRDS(bart_ps_model_final, paste0(output_path, "/ps_model/bart_ps_model_final.rds"))
 
 }
 
 
-# 
+
 # predictions <- bart_ps_model_final$p_hat_train
 # 
 # df <- as.data.frame(cbind(predictions, ps.model.train["drugclass"] %>%
@@ -204,86 +175,86 @@ if (class(try(
 #   set_names(c("predictions", "labels")) %>%
 #   mutate(predictions = ifelse(predictions > values$threshold, 1, 2)) %>% table()
 # 
-
-
-###############################################################################
-###############################################################################
-###################### Propensity scores for patients #########################
-###############################################################################
-###############################################################################
-
-
-# Prop scores for train dataset
-patient_prop_scores <- ps.model.train %>%
-  select(patid, pated) %>%
-  cbind(prop.score = bart_ps_model_final$p_hat_train)
-
-
-# Prop scores for test dataset
-
-ps.model.test <- set_up_data_sglt2_glp1(dataset.type = "ps.model.test")
-
-# calculate prop score
-if (class(try(
-
-  prop_score_testing_data <- readRDS(paste0(output_path, "/ps_model/prop_score_testing_data.rds"))
-
-  , silent = TRUE)) == "try-error") {
-
-  prop_score_testing_data <- predict(bart_ps_model_final, ps.model.test %>%
-                        select(
-                          colnames(bart_ps_model_final$X)
-                        ))
-
-  saveRDS(prop_score_testing_data, paste0(output_path, "/ps_model/prop_score_testing_data.rds"))
-
-}
-
-
-
-# predictions <- prop_score_testing_data
 # 
-# df <- as.data.frame(cbind(predictions, ps.model.test["drugclass"] %>%
-#                             mutate(drugclass = ifelse(drugclass == "SGLT2", 0, 1)))) %>%
-#   set_names(c("predictions", "labels"))
+#
+# ###############################################################################
+# ###############################################################################
+# ###################### Propensity scores for patients #########################
+# ###############################################################################
+# ###############################################################################
 # 
-# library(ROCR)
-# pred <- prediction(df$predictions, df$labels)
-# perf <- performance(pred,"tpr","fpr")
-# pdf()
-# plot(perf,colorize=TRUE)
-# dev.off()
 # 
-# library(pROC)
-# pROC_obj <- roc(df$labels,df$predictions,
-#                 smoothed = TRUE,
-#                 # arguments for ci
-#                 ci=TRUE, ci.alpha=0.9, stratified=FALSE,
-#                 # arguments for plot
-#                 plot=TRUE, auc.polygon=TRUE, max.auc.polygon=TRUE, grid=TRUE,
-#                 print.auc=TRUE, show.thres=TRUE)
+# # Prop scores for train dataset
+# patient_prop_scores <- ps.model.train %>%
+#   select(patid, pated) %>%
+#   cbind(prop.score = bart_ps_model_final$p_hat_train)
+# 
+# 
+# # Prop scores for test dataset
+# 
+# ps.model.test <- set_up_data_sglt2_glp1(dataset.type = "ps.model.test")
+# 
+# # calculate prop score
+# if (class(try(
+# 
+#   prop_score_testing_data <- readRDS(paste0(output_path, "/ps_model/prop_score_testing_data.rds"))
+# 
+#   , silent = TRUE)) == "try-error") {
+# 
+#   prop_score_testing_data <- predict(bart_ps_model_final, ps.model.test %>%
+#                         select(
+#                           colnames(bart_ps_model_final$X)
+#                         ))
+# 
+#   saveRDS(prop_score_testing_data, paste0(output_path, "/ps_model/prop_score_testing_data.rds"))
+# 
+# }
 # 
 # 
 # 
-# # values <- coords(pROC_obj, "best", ret=c("threshold", "specificity", "sensitivity", "accuracy",
-# #                                          "precision", "recall"), transpose = FALSE)
+# # predictions <- prop_score_testing_data
+# # 
+# # df <- as.data.frame(cbind(predictions, ps.model.test["drugclass"] %>%
+# #                             mutate(drugclass = ifelse(drugclass == "SGLT2", 0, 1)))) %>%
+# #   set_names(c("predictions", "labels"))
+# # 
+# # library(ROCR)
+# # pred <- prediction(df$predictions, df$labels)
+# # perf <- performance(pred,"tpr","fpr")
+# # pdf()
+# # plot(perf,colorize=TRUE)
+# # dev.off()
+# # 
+# # library(pROC)
+# # pROC_obj <- roc(df$labels,df$predictions,
+# #                 smoothed = TRUE,
+# #                 # arguments for ci
+# #                 ci=TRUE, ci.alpha=0.9, stratified=FALSE,
+# #                 # arguments for plot
+# #                 plot=TRUE, auc.polygon=TRUE, max.auc.polygon=TRUE, grid=TRUE,
+# #                 print.auc=TRUE, show.thres=TRUE)
+# # 
+# # 
+# # 
+# # # values <- coords(pROC_obj, "best", ret=c("threshold", "specificity", "sensitivity", "accuracy",
+# # #                                          "precision", "recall"), transpose = FALSE)
+# # 
+# # 
+# # confusion.matrix.val <- as.data.frame(cbind(predictions, ps.model.test[,"drugclass"])) %>%
+# #   set_names(c("predictions", "labels")) %>%
+# #   mutate(predictions = ifelse(predictions > values$threshold, 1, 2)) %>% table()
 # 
 # 
-# confusion.matrix.val <- as.data.frame(cbind(predictions, ps.model.test[,"drugclass"])) %>%
-#   set_names(c("predictions", "labels")) %>%
-#   mutate(predictions = ifelse(predictions > values$threshold, 1, 2)) %>% table()
-
-
-
-
-patient_prop_scores <- patient_prop_scores %>%
-  rbind(
-    ps.model.test %>%
-      select(patid, pated) %>%
-      cbind(prop.score = prop_score_testing_data)
-  ) %>%
-  as.data.frame()
-
-saveRDS(patient_prop_scores, paste0(output_path, "/ps_model/patient_prop_scores.rds"))
-
+# 
+# 
+# patient_prop_scores <- patient_prop_scores %>%
+#   rbind(
+#     ps.model.test %>%
+#       select(patid, pated) %>%
+#       cbind(prop.score = prop_score_testing_data)
+#   ) %>%
+#   as.data.frame()
+# 
+# saveRDS(patient_prop_scores, paste0(output_path, "/ps_model/patient_prop_scores.rds"))
+# 
 
